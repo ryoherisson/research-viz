e19d1e7760b0d74298d444b0cbb7df6baeb29cd1
ryoherisson <ryhizw@gmail.com>
2023-09-23 19:41:04 +0900

    update plot for florida

--- gcsb-viz/plot_memorysize.py
+++ gcsb-viz/plot_memorysize.py
@@ -1,11 +1,14 @@
+import itertools
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-platform = 'A100'
+matrix = 'florida'
+platform = 'titan'
 
-MEMORYSIZE_FILENAME = f'data/input/random_memorysize_{platform}.csv'
-OUTPUT_FILEPATH = f'data/output/random_memorysize_{platform}.png'
+MEMORYSIZE_FILENAME = f'data/input/{matrix}_memorysize_{platform}.csv'
+OUTPUT_FILEPATH = f'data/output/{matrix}_memorysize_{platform}.png'
 
 color=['royalblue', 'orange', 'grey', 'limegreen']
 
@@ -22,24 +25,38 @@ def reshape_df(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
 
 def main():
     memorysize_df = read_csv(MEMORYSIZE_FILENAME)
-    memorysize_df = reshape_df(memorysize_df, 'memorysize')
+    n_matrices = len(memorysize_df.index)
+    memorysize_df_reshaped = reshape_df(memorysize_df, 'memorysize')
 
-    plt.figure(figsize=(25, 9))
+    plt.figure(figsize=(36, 9))
+    # plt.figure(figsize=(25, 12))
 
     # barを描画
-    sns.barplot(data=memorysize_df, x='matrix', y='memorysize', hue='sparse_matrix_format', palette=color)
+    bar = sns.barplot(data=memorysize_df_reshaped, x='matrix', y='memorysize', hue='sparse_matrix_format', palette=color)
+
+    hatches = itertools.cycle(['/', '+', 'o', 'x', '\\', '*', 'o', 'O', '.'])
+
+    # Loop over the bars
+    for i, thisbar in enumerate(bar.patches):
+        # Set a different hatch for each bar
+        if i % n_matrices == 0:
+            hatch = next(hatches)
+        thisbar.set_hatch(hatch)
 
     # 文字サイズを大きくする
     plt.title(platform, fontsize=20)
 
-    # y軸の値の文字サイズを大きくする
+    # xy軸の値の文字サイズを大きくする
     plt.yticks(fontsize=16)
+    plt.xticks(fontsize=16)
+    # plt.xticks(fontsize=16, rotation=90)
 
     plt.ylabel('Relative Memory Size Over CSR', fontsize=20)
     # x軸のラベルを表示しない
     plt.xlabel('')
     # labelを下部に移動. 枠線を消す
     plt.legend(bbox_to_anchor=(0.5, -0.1), loc='upper center', ncol=4, fontsize=20, frameon=False)
+    # plt.legend(bbox_to_anchor=(0.5, -0.2), loc='upper center', ncol=4, fontsize=20, frameon=False)
 
 
     # 余白を調整
--- gcsb-viz/plot_speedup.py
+++ gcsb-viz/plot_speedup.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-platform = 'A100'
+matrix = 'florida'
+platform = 'titan'
 
-SPEEDUP_FILENAME = f'data/input/random_speedup_{platform}.csv'
-OUTPUT_FILEPATH = f'data/output/random_speedup_{platform}.png'
+SPEEDUP_FILENAME = f'data/input/{matrix}_speedup_{platform}.csv'
+OUTPUT_FILEPATH = f'data/output/{matrix}_speedup_{platform}.png'
 
 color=['royalblue', 'orange', 'grey', 'limegreen']
 
@@ -23,25 +24,32 @@ def reshape_df(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
 def main():
     speedup_df = read_csv(SPEEDUP_FILENAME)
 
-    speedup_df = reshape_df(speedup_df, 'speedup')
+    # speedup_df = reshape_df(speedup_df, 'speedup')
 
-    plt.figure(figsize=(25, 9))
+    plt.figure(figsize=(36, 9))
+    # plt.figure(figsize=(25, 12))
 
     # barを描画
-    sns.barplot(data=speedup_df, x='matrix', y='speedup', hue='sparse_matrix_format', palette=color)
+    # sns.barplot(data=speedup_df, x='matrix', y='speedup', hue='sparse_matrix_format', palette=color)
+
+    sns.lineplot(data=speedup_df, palette=color, markers=True, markersize=10, linewidth=3)
 
     # 文字サイズを大きくする
     plt.title(platform, fontsize=20)
 
-    # y軸の値の文字サイズを大きくする
+    # xy軸の値の文字サイズを大きくする
     plt.yticks(fontsize=16)
+    plt.xticks(fontsize=16)
+    # plt.xticks(fontsize=16, rotation=90)
 
     # y軸のラベルを大きくする
     plt.ylabel('Relative Speed up Over CSR', fontsize=20)
     # x軸のラベルを表示しない
     plt.xlabel('')
     # labelを下部に移動. 枠線を消す
+    # xticksと被らないようにbbox_to_anchorの値を調整
     plt.legend(bbox_to_anchor=(0.5, -0.1), loc='upper center', ncol=4, fontsize=20, frameon=False)
+    # plt.legend(bbox_to_anchor=(0.5, -0.2), loc='upper center', ncol=4, fontsize=20, frameon=False)
 
 
     # 余白を調整
